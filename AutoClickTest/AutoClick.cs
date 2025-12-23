978dd408c7d0de5f2ce48b6defa92848517181bd
howard626 <89678859+howard626@users.noreply.github.com>
2025-12-23 15:43:16 +0800

    Update AutoClick.cs to use ActionExecutor and add Pause/Resume handlers.

--- AutoClickTest/AutoClick.cs
+++ AutoClickTest/AutoClick.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +9,7 @@ namespace AutoClickTest
     public partial class AutoClick : Form
     {
         /// <summary>
-        /// 是否執行
+        /// 是否執行 (legacy flag still present for a quick check)
         /// </summary>
         private bool IsStart = false;
 
@@ -23,30 +23,22 @@ namespace AutoClickTest
         /// </summary>
         private int ActionCount { get; set; }
 
+        private ActionExecutor _executor;
+
         public AutoClick()
         {
             InitializeComponent();
         }
 
-        /// <summary>
-        /// 載入事件
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void AutoClick_Load(object sender, EventArgs e)
         {
 
         }
-        /// <summary>
-        /// 增加排程動作
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+
         private void AddAction(object sender, EventArgs e)
         {
             AddForm addDialog = new AddForm();
 
-            // Show testDialog as a modal dialog and determine if DialogResult = OK.
             if (addDialog.ShowDialog(this) == DialogResult.OK)
             {
                 addDialog.Action.ID = ActionCount++;
@@ -76,7 +68,7 @@ namespace AutoClickTest
                             ActionList.Nodes.Add($"在 {addDialog.GetPoint(mouse)} {mouse.Action_Desc}--延遲 {mouse.Delay_MS} 秒");
                             break;
                     }
-                    
+
                 }
                 txtResult.Text = "Ok";
             }
@@ -86,121 +78,56 @@ namespace AutoClickTest
             }
             addDialog.Dispose();
         }
-        /// <summary>
-        /// 開始按鈕
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+
         private void Start_Click(object sender, EventArgs e)
         {
+            if (Actions.Count == 0)
+            {
+                MessageBox.Show("請先新增動作");
+                return;
+            }
+
             IsStart = true;
-            Run();
-        }
-        
-        /// <summary>
-        /// 開始執行程序
-        /// </summary>
-        private void Run()
-        {
-            int delaySec = 0;
-            for (int i = 0; i < Actions.Count; i++)
+
+            // create executor if null
+            if (_executor == null)
             {
-                delaySec += Actions[i].Delay_MS;
-                if (i == Actions.Count - 1)
-                    Do(Actions[i], delaySec, Actions[i].GetType(), true); 
-                else
-                    Do(Actions[i], delaySec, Actions[i].GetType());
+                _executor = new ActionExecutor(Actions);
+                _executor.OnStatus += (s) => { this.BeginInvoke((Action)(() => { txtResult.Text = s; })); };
             }
+
+            // read repeat count from UI if present
+            try
+            {
+                _executor.SequenceRepeat = (int)numericUpDownRepeat.Value;
+            }
+            catch { _executor.SequenceRepeat = 1; }
+
+            _executor.Start();
         }
 
-        /// <summary>
-        /// 開始執行動作
-        /// </summary>
-        private async void Do(Action action, int delaySec, Type type, bool end = false)
+        private void Pause_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            await Task.Delay(delaySec + random.Next(-100, 100));
-            if (IsStart)
-            {
-                if (type == typeof(KeyCodeAction))
-                {
-                    KeyCodeAction key = (KeyCodeAction)action;
-                    switch (key.Action_Desc)
-                    {
-                        case "按一下":
-                            Keybord.Press(key.KeyCode.ToUpper());
-                            break;
-                        case "按住":
-                            Keybord.Hold(key.KeyCode.ToUpper(), 5000);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                else if (type == typeof(MouseAction))
-                {
-                    MouseAction mouse = (MouseAction)action;
-                    switch (mouse.Action_Desc)
-                    {
-                        case "點一下左鍵":
-                            Mouse.Move(mouse.X + random.Next(-10, 10), mouse.Y + random.Next(-10, 10));
-                            Mouse.LeftClick();
-                            break;
-                        case "點兩下左鍵":
-                            Mouse.Move(mouse.X + random.Next(-10, 10), mouse.Y + random.Next(-10, 10));
-                            Mouse.LeftClick();
-                            Thread.Sleep(50);
-                            Mouse.LeftClick();
-                            break;
-                        case "點一下右鍵":
-                            Mouse.Move(mouse.X + random.Next(-10, 10), mouse.Y + random.Next(-10, 10));
-                            Mouse.RightClick();
-                            break;
-                        case "點一下中鍵":
-                            Mouse.Move(mouse.X + random.Next(-10, 10), mouse.Y + random.Next(-10, 10));
-                            Mouse.MiddleClick();
-                            break;
-                        case "前滾":
-                            for (int j = 0; j < mouse.X; j++)
-                            {
-                                Mouse.Roll(Mouse.ROLLWAY.FRONT);
-                            }
-                            break;
-                        case "後滾":
-                            for (int j = 0; j < mouse.X; j++)
-                            {
-                                Mouse.Roll(Mouse.ROLLWAY.BACK);
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                if (end && IsStart)
-                {
-                    Run();
-                }
-            }
+            _executor?.Pause();
         }
 
-        /// <summary>
-        /// 停止按鈕
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        private void Resume_Click(object sender, EventArgs e)
+        {
+            _executor?.Resume();
+        }
+
         private void Stop_Click(object sender, EventArgs e)
         {
             IsStart = false;
+            _executor?.Stop();
         }
 
         private void Test_Click(object sender, EventArgs e)
         {
             BackGround b = new BackGround();
 
-            // Show testDialog as a modal dialog and determine if DialogResult = OK.
             if (b.ShowDialog(this) == DialogResult.OK)
             {
-                // Read the contents of testDialog's TextBox.
                 txtResult.Text = $"{b.Point.X}, {b.Point.Y}";
             }
             else
@@ -220,16 +147,13 @@ namespace AutoClickTest
             }
         }
 
-        /// <summary>
-        /// 清除按鈕
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Clear_Click(object sender, EventArgs e)
         {
             IsStart = false;
             Actions.Clear();
             ActionList.Nodes.Clear();
+            _executor?.Stop();
+            _executor = null;
         }
     }
 }
